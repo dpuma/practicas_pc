0172de468c188df50bee251facbb182125a76e3d
dpuma <dpumar3@gmail.com>
2021-12-16 06:13:21 -0500

    suffix_array

--- prac_pc_profAlex/ejercicios clase 2021-12-02/suffixArray.cpp
+++ prac_pc_profAlex/ejercicios clase 2021-12-02/suffixArray.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 int main()
 {
-    map<string,int> prefijos;
+    map<string,int> sufijos;
     string palabra;
     cin >> palabra;
 
     for(int i = 0; i < palabra.size(); i++)
-        prefijos.insert(make_pair(palabra.substr(i), i));
+        sufijos.insert(make_pair(palabra.substr(i), i));
 
-    for (auto it : prefijos)
-        cout << it.second << "," << it.first << endl; // Sorted
+    for (auto it : sufijos)
+        cout << it.second << "," << it.first << endl;
 
     return 0;
 }
